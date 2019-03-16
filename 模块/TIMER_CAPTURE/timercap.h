9db676d73b4bd92c2eb7f2f11640299207951343
qxdn <1464238196@qq.com>
2019-03-16 15:30:40 +0800

    fix bug

--- 模块/TIMER_CAPTURE/timercap.h
+++ 模块/TIMER_CAPTURE/timercap.h
@@ -12,8 +12,8 @@
 //[7] 1成功捕获高电平一次  0未捕获
 //[6] 1捕获到上升沿 0未捕获
 //[5:0] 捕获到上升沿后溢出个数
-u8 TIM5CH1_CAPTURE_STA = 0; //输入捕获状态
-u32 TIM5CH1_CAPTURE_VAL;    //输入捕获值 TIM2/TIM5 32位
+extern  u8 TIM5CH1_CAPTURE_STA = 0; //输入捕获状态
+extern  u32 TIM5CH1_CAPTURE_VAL;    //输入捕获值 TIM2/TIM5 32位
 
 
 void TIM5_CH1_Cap_Init(u16 psc, u16 arr);  //初始化函数
