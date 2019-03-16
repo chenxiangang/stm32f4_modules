88e899f15d9b1223777d41bbb0353189cf2446dd
while0l1 <1255015689@qq.com>
2019-03-16 16:37:24 +0800

    修改readme,增加.h函数里面的void

--- Debuging/Line_CCD/CCD.h
+++ Debuging/Line_CCD/CCD.h
@@ -10,18 +10,18 @@
 #include "sys.h"
 
 #define CCD_CLK PCout(1) //时钟输入 PC1
-#define CCD_SI PCout(2)  //开始信号 PC2
+#define CCD_SI PCout(2) //开始信号 PC2
 
-extern int CCD_Value[128] = {0};     //CCD的值 不带DMA
-extern u16 DMA_CCD_Value[128] = {0}; //CCD的值 使用DMA
-extern int CCD_Yuzhi;                //CCD阈值
-extern int CCD_Zhongzhi;             //CCD中线
+extern int CCD_Value[128]; //CCD的值 不带DMA
+extern u16 DMA_CCD_Value[128]; //CCD的值 使用DMA
+extern int CCD_Yuzhi; //CCD阈值
+extern int CCD_Zhongzhi; //CCD中线
 
-void CCD_Init_ND(void);  //CCD初始化没有DMA
-u16 Get_Adc(u8 ch);      //ADC的值 无DMA
-void read_CCD_ND(void);  //CCD的数据采集
-void CCD_Find_Mid_Nd();  //找中线 无CCD
+void CCD_Init_ND(void); //CCD初始化没有DMA
+u16 Get_Adc(u8 ch); //ADC的值 无DMA
+void read_CCD_ND(void); //CCD的数据采集
+void CCD_Find_Mid_Nd(void); //找中线 无CCD
 void CCD_Init_DMA(void); //DMA_CCD初始化
-void DMA_Read_CCD();     //DMA_CCD读值
-void DMA_CCD_Find_Mid(); //DMA_CCD找中线
+void DMA_Read_CCD(void); //DMA_CCD读值
+void DMA_CCD_Find_Mid(void); //DMA_CCD找中线
 #endif
--- Debuging/Line_CCD/readme.md
+++ Debuging/Line_CCD/readme.md
@@ -11,3 +11,17 @@
 - CLK   PC1 
 - SI    PC2
 - AO    PC3
+
+## 工程构建
+- 把`Line_CCD`复制到工程目录下
+- 在工程中添加.c文件 `stm32f4xx_adc.c`和`stm32f4xx_dma.c`
+- 在工程中添加`Line_CCD`的头文件路径
+- 在`main.c`里面添加`CCD.h`
+- 先加入`CCD_Init_ND()`初始化
+- 直接可以读到128个值
+  ```
+  for(a=0; a<128; a++)
+  {
+	  printf("%d ",CCD_Value[a]);
+  }
+    ```
