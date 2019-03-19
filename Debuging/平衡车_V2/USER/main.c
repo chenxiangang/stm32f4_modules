e6439e7409debeaf0be9e0083adaa613d4532ce0
while0l1 <1255015689@qq.com>
2019-03-19 22:02:50 +0800

    添加陀螺仪

--- Debuging/平衡车_V2/USER/main.c
+++ Debuging/平衡车_V2/USER/main.c
@@ -1,15 +1,15 @@
-#include "sys.h"
-#include "delay.h"
-#include "usart.h"
-#include "led.h"
-#include "YL70.h"
-#include "encoder.h"
-#include "JY901_uart.h"
-#include "include.h"
 #include "FreeCars_uart.h"
-#include "mynvic.h"
+#include "JY901_uart.h"
+#include "YL70.h"
 #include "control.h"
+#include "delay.h"
+#include "encoder.h"
+#include "include.h"
+#include "led.h"
+#include "mynvic.h"
+#include "sys.h"
 #include "tb6612.h"
+#include "usart.h"
 
 /*
 光电对管： PD0 PD1 PD2 PD3 
@@ -28,29 +28,22 @@ OLED:   CS  PD3
 FLAG_Typedef flag;
 
 int main(void)
-{ 
- 
-	delay_init(168);		  //初始化延时函数
-	LED_Init();		        //初始化LED端口
-	YL_70_Init();         //初始化光电对管
-	TIM5_Init(100,7199);   //读取传感器数据，进行pid控制
-	Encoder_TIM4_Init();  //初始化电机编码器A
-	Encoder_TIM2_Init();
-	uart_init(115200);    //初始化串口1，用于发送数据到上位机
-	usart3_init(115200);  //用来读取陀螺仪的数据
-	My_NVIC_Init();
-	TB6612_Init();
-	
-	
-	
-	while(1)
-	{
-		speedcontrol(1000,1);
-		speedcontrol(1000,2);
-	}
+{
+
+    delay_init(168); //初始化延时函数
+    LED_Init(); //初始化LED端口
+    YL_70_Init(); //初始化光电对管
+    Encoder_TIM2_Init();
+    Encoder_TIM4_Init(); //初始化电机编码器A
+    //TIM5_Init(100, 7199); //读取传感器数据，进行pid控制
+    uart_init(115200); //初始化串口1，用于发送数据到上位机
+    usart3_init(115200); //用来读取陀螺仪的数据
+    My_NVIC_Init();
+    TB6612_Init();
+
+    while (1) {
+        printf("%f", roll);
+        // speedcontrol(1000,1);
+        // speedcontrol(1000,2);
+    }
 }
-
-
-
-
-
